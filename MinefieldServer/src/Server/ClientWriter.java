055d62d4276d4cdf4c149ca5a1e9870dfc4f3730
NerfIrelia <looperalexander@gmail.com>
2015-03-20 15:56:55 -0400

    Added a random map selector
    Created a new class that scans through MAPS, then pulls a random text
    file.  Should be dynamic, just add new text files to MAPS and nothing
    else.

--- MinefieldServer/src/Server/ClientWriter.java
+++ MinefieldServer/src/Server/ClientWriter.java
@@ -2,10 +2,13 @@
 package Server;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Random;
 
 import GameMechanics.Map;
 import GameMechanics.MineCreation;
+import Main.FileReading;
 
 /**
  * This class sets up all of the appropriate data 
@@ -16,167 +19,198 @@ import GameMechanics.MineCreation;
  */
 public class ClientWriter implements Runnable {
 
-    private ArrayList<Client> clients = new ArrayList<Client>(); //The clients contained within this thread
-    private Map map = new Map(new File("MAPS/bridges.txt"));
-    private Map mineLayer;
-    private double minePercentage = 0.10;
-    
-    private final int raceTime = 5 * 60 * 1000; //5 Minutes
-    private final int lobbyTime = 5*1000;//30 * 1000;	//30 Seconds
-    private int currentTime = lobbyTime;				//Time counter
-    private boolean inRace = false;				//Race = true, lobby = false;
-    
-    private boolean someoneWon = false;			//Temporary variable
-    
-    
-    /**
-     * The default constructor (not in use)
-     */
-    public ClientWriter(){}
-    
-    /**
-     * Returns the list of clients in this thread
-     * @return 
-     *          The list of clients in this thread
-     */
-    public ArrayList<Client> clients(){
-        return this.clients;
-    }
-    
-    /**
-     * The thread: Compiles all the 
-     * necessary data and sends it to 
-     * all current clients.
-     */
-    public void run() {
-        while(true){
-            
-            checkClients();
-            ArrayList<Client> temp = clients;
-            
-            for(int w = 0; w < temp.size(); w++){
-                try{
-                    if(temp.get(w).canGetInfo())
-                        temp.get(w).setData(setupData(w));
-                }catch(IndexOutOfBoundsException e){}
-            }
-            checkClients();
-            sleep(2); //5 ms
-            switchModes();
-        }
-    }
-    
-    /**
-     * This determines if a mode switch is necessary. If it is, it does it.
-     */
-    private void switchModes(){
-    	if(this.clients().size()>1 || inRace)
-    		currentTime-=2;//5 ms
-    	if(this.clients().size() == 0){
-    		this.inRace = false;
-    		this.currentTime = lobbyTime;
-    		this.someoneWon = false;
-    	}
-    	
-    	if(this.currentTime <= 0 && !inRace){
-    		System.out.println("STARTING RACE");
-    		this.inRace = true;
-    		this.currentTime = raceTime;
-    		
-    		//System.out.println(map.toString());
-    		
-    		//Start race
-    		//Populate mineLayer with mines and numbers
-    		mineLayer = MineCreation.createMineLayer(map.map, minePercentage);
-    		
-    		//System.out.println(mineLayer.toString());
-    		
-    		//Place all current clients at the starting line
-    		for(int c = 0; c < this.clients.size(); c++){
-    			if(this.clients.get(c).canRace()){
-    				this.clients.get(c).setSpectatorMode(false);
-    				this.clients.get(c).player().setX(1);
-    				this.clients.get(c).player().setY(((int)((Math.random()*100)%(this.map.getHeight()-2)))+1);
-    				
-    			}
-    		}
-    		
-    	}else if(this.currentTime <= 0 && inRace){
-    		System.out.println("ENTERING LOBBY.");
-    		this.inRace = false;
-    		this.currentTime = lobbyTime;
-    		this.someoneWon = false;
-    		
-    		//Start Lobby
-    		for(int c = 0; c < this.clients.size(); c++){
+	private ArrayList<Client> clients = new ArrayList<Client>(); //The clients contained within this thread
+	private Map map = new Map(new File("MAPS/bridges.txt"));
+	private Map mineLayer;
+	private static Map mapArray[];
+	private double minePercentage = 0.10;
+
+	private final int raceTime = 5 * 60 * 1000; //5 Minutes
+	private final int lobbyTime = 5*1000;//30 * 1000;	//30 Seconds
+	private int currentTime = lobbyTime;				//Time counter
+	private boolean inRace = false;				//Race = true, lobby = false;
+
+	private boolean someoneWon = false;			//Temporary variable
+
+
+	/**
+	 * The default constructor (not in use)
+	 */
+	public ClientWriter(){
+		FileReading temp = new FileReading();
+		File f  [] = temp.getMaps();
+		mapArray = new Map[f.length];
+		for(int i = 0; i < 12; i++){
+			mapArray[i] = new Map(f[i]);
+		}
+		//create random number
+		int rndm = getRandomNumber();
+		System.out.println(rndm);
+		//choose random map
+		map = mapArray[rndm];
+	}
+
+
+
+	/**
+	 * Returns the list of clients in this thread
+	 * @return 
+	 *          The list of clients in this thread
+	 */
+	public ArrayList<Client> clients(){
+		return this.clients;
+	}
+
+	/**
+	 * The thread: Compiles all the 
+	 * necessary data and sends it to 
+	 * all current clients.
+	 */
+	public void run() {
+		while(true){
+
+			checkClients();
+			ArrayList<Client> temp = clients;
+
+			for(int w = 0; w < temp.size(); w++){
+				try{
+					if(temp.get(w).canGetInfo())
+						temp.get(w).setData(setupData(w));
+				}catch(IndexOutOfBoundsException e){}
+			}
+			checkClients();
+			sleep(2); //5 ms
+			switchModes();
+		}
+	}
+
+	/**
+	 * This determines if a mode switch is necessary. If it is, it does it.
+	 */
+	private void switchModes(){
+		if(this.clients().size()>1 || inRace)
+			currentTime-=2;//5 ms
+		if(this.clients().size() == 0){
+			this.inRace = false;
+			this.currentTime = lobbyTime;
+			this.someoneWon = false;
+		}
+
+		if(this.currentTime <= 0 && !inRace){
+			System.out.println("STARTING RACE");
+			this.inRace = true;
+			this.currentTime = raceTime;
+
+			//System.out.println(map.toString());
+
+			//Start race
+			//Populate mineLayer with mines and numbers
+			mineLayer = MineCreation.createMineLayer(map.map, minePercentage);
+
+			//System.out.println(mineLayer.toString());
+
+			//Place all current clients at the starting line
+			for(int c = 0; c < this.clients.size(); c++){
+				if(this.clients.get(c).canRace()){
+					this.clients.get(c).setSpectatorMode(false);
+					this.clients.get(c).player().setX(1);
+					this.clients.get(c).player().setY(((int)((Math.random()*100)%(this.map.getHeight()-2)))+1);
+
+				}
+			}
+
+		}else if(this.currentTime <= 0 && inRace){
+			System.out.println("ENTERING LOBBY.");
+			//create random number
+			int rndm = getRandomNumber();
+			System.out.println(rndm);
+			//choose random map
+			map = mapArray[rndm];
+
+			this.inRace = false;
+			this.currentTime = lobbyTime;
+			this.someoneWon = false;
+
+			//Start Lobby
+			for(int c = 0; c < this.clients.size(); c++){
 				this.clients.get(c).setSpectatorMode(true);
-    		}
-    	}
-    	
-    	if(inRace && this.currentTime == 10000){
-    		System.out.println("RETURNING TO LOBBY IN 10 SECONDS.");
-    	}
-    	if(!inRace && this.currentTime == 10000){
-    		System.out.println("STARTING RACE IN 10 SECONDS.");
-    	}
-    }
-    
-    /**
-     * Double checks to see if all clients 
-     * in the client list are still there. 
-     * Removes them as necessary
-     */
-    public void checkClients(){
-        try{
-            for(int cl = 0; cl < clients.size(); cl++){
-                if(!clients.get(cl).isActive()){
-                    clients.remove(cl);
-                    cl--;
-                }else{ //If the client exists
-                	clients.get(cl).updateMap(map);
-                	clients.get(cl).updateMineLayer(mineLayer);
-                	clients.get(cl).setTime(this.formatTime());
-                	if(!this.inRace)
-                		clients.get(cl).setCanRace(true);
-                	
-                	//If a client met win Condition
-                	if(checkWinCondition(cl) && !this.someoneWon){
-                		this.currentTime = 10*1000; //Ten seconds left
-                		this.someoneWon = true;
-                	}
-                	
-                	//Check to see if client has hit a mine
-                	if(clients.get(cl).mineHit()){
-                		this.mineHit(cl);
-                	}
-                }
-            }
-        }catch(NullPointerException e){
-            System.out.println("NullPointer in check clients.");
-            e.printStackTrace();
-        }
-    }
-    
-    /**
-     * This method takes the current time and converts 
-     * it into a nice string to pass to the client
-     * @return
-     * 			A formatted time string
-     */
-    public String formatTime(){   
-    	String time = "";
-    	int min = this.currentTime/60000;
-    	int sec = this.currentTime/1000 - min*60;
-    	int mil = this.currentTime%1000;
-    	
-    	if(min < 1){
-    		time+="0:";
-    	}else{
-    		time+=""+min+":";
-    	}
-    	
-    	if(sec==0){
-    		time+="00:";
+			}
+		}
+
+		if(inRace && this.currentTime == 10000){
+			System.out.println("RETURNING TO LOBBY IN 10 SECONDS.");
+		}
+		if(!inRace && this.currentTime == 10000){
+			System.out.println("STARTING RACE IN 10 SECONDS.");
+		}
+	}
+
+	/**
+	 * Double checks to see if all clients 
+	 * in the client list are still there. 
+	 * Removes them as necessary
+	 */
+
+	public static int getRandomNumber(){
+		Random rand = new Random();
+
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum = rand.nextInt((mapArray.length -1 ));
+
+		return randomNum;
+	}
+	public void checkClients(){
+		try{
+			for(int cl = 0; cl < clients.size(); cl++){
+				if(!clients.get(cl).isActive()){
+					clients.remove(cl);
+					cl--;
+				}else{ //If the client exists
+					clients.get(cl).updateMap(map);
+					clients.get(cl).updateMineLayer(mineLayer);
+					clients.get(cl).setTime(this.formatTime());
+					if(!this.inRace)
+						clients.get(cl).setCanRace(true);
+
+					//If a client met win Condition
+					if(checkWinCondition(cl) && !this.someoneWon){
+						this.currentTime = 10*1000; //Ten seconds left
+						this.someoneWon = true;
+					}
+
+					//Check to see if client has hit a mine
+					if(clients.get(cl).mineHit()){
+						this.mineHit(cl);
+					}
+				}
+			}
+		}catch(NullPointerException e){
+			System.out.println("NullPointer in check clients.");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method takes the current time and converts 
+	 * it into a nice string to pass to the client
+	 * @return
+	 * 			A formatted time string
+	 */
+	public String formatTime(){   
+		String time = "";
+		int min = this.currentTime/60000;
+		int sec = this.currentTime/1000 - min*60;
+		int mil = this.currentTime%1000;
+
+		if(min < 1){
+			time+="0:";
+		}else{
+			time+=""+min+":";
+		}
+
+		if(sec==0){
+			time+="00:";
 		}else if(sec<10){
 			time+="0"+sec+":";
 		}else{
@@ -232,7 +266,7 @@ public class ClientWriter implements Runnable {
 	}
 
 	public void mineHit(int ClientNumber) {
-		
+
 		int explosionX = clients.get(ClientNumber).player().getX();
 		int explosionY = clients.get(ClientNumber).player().getY();
 
@@ -249,13 +283,13 @@ public class ClientWriter implements Runnable {
 		for (int k = 0; k < clients.size(); k++) {
 			// Checks if players are in this radius
 			if (clients.get(k).player().getX() >= explosionXmin && clients.get(k).player().getX() <= explosionXmax
-			 && clients.get(k).player().getY() >= explosionYmin && clients.get(k).player().getY() <= explosionYmax) {
-				
+					&& clients.get(k).player().getY() >= explosionYmin && clients.get(k).player().getY() <= explosionYmax) {
+
 				// Sets players back to start and resets the mineHit variable in Client.
 				// Currently just returns player to x = 1, remove when fixed.
 				clients.get(k).player().setX(1);
 				clients.get(k).player().setY(((int)((Math.random()*100)%(this.map.getHeight()-2)))+1);
-        		clients.get(k).setMineHit(false);
+				clients.get(k).setMineHit(false);
 			}
 		}
 	}
